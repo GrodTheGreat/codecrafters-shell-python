432a1b4e1d9d98bafacb1636c8a9b6fcaf112ff6
Gerard Causse <grod.causse@gmail.com>
2026-03-24 00:07:01 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 import sys
 
+VALID_COMMANDS = []
+
 
 def main():
     sys.stdout.write("$ ")
+    command = input()
+    if command not in VALID_COMMANDS:
+        print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
